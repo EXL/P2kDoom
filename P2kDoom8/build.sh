78bc89a437fabb29cc7b564214c652af09eaec58
fk <nzmzrta@gmail.com>
2025-09-09 18:18:16 +0300

    [P2kDoom8]: Support 176x220 dimension build for Argon

--- P2kDoom8/build.sh
+++ P2kDoom8/build.sh
@@ -56,7 +56,7 @@ build_rainbow() {
 
 build_argon() {
 	echo -e "\033[32mBuilding for P2K/Argon...\033[0m"
-	for res in "240x160" "240x320" "240x320E" "320x240"; do
+	for res in "240x160" "176x220" "220x176" "240x320" "240x320E" "320x240"; do
 		make -f Makefile.ea1 clean
 		RES="$res" GFX=NVIDIA QUALITY=MID make -f Makefile.ea1
 		mv *.elf "Release/Argon" 2>/dev/null || true
